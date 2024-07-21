959ffe1b3a8c1a999ef4f993ae6d362d9245e5cf
AlpayKoyuncuoglu <41324656+AlpayKoyuncuoglu@users.noreply.github.com>
2024-07-21 19:31:19 +0300

    Meetings Controller made

--- MeetingProject.Context/Migrations/20240721162357_Meeting.Designer.cs
+++ MeetingProject.Context/Migrations/20240721162357_Meeting.Designer.cs
@@ -4,6 +4,7 @@ using MeetingProject.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MeetingProject.Context.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240721162357_Meeting")]
+    partial class Meeting
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MeetingProject.Web/Controllers/MeetingsController.cs
@@ -0,0 +1,97 @@
+﻿using MeetingProject.Context;
+using MeetingProject.Model.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MeetingProject.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MeetingsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MeetingsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Meeting>>> GetMeetings()
+        {
+            return await _context.Meetings.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Meeting>> GetMeeting(int id)
+        {
+            var meeting = await _context.Meetings.FindAsync(id);
+
+            if (meeting == null)
+            {
+                return NotFound();
+            }
+
+            return meeting;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Meeting>> PostMeeting(Meeting meeting)
+        {
+            _context.Meetings.Add(meeting);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetMeeting", new { id = meeting.Id }, meeting);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMeeting(int id, Meeting meeting)
+        {
+            if (id != meeting.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(meeting).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MeetingExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMeeting(int id)
+        {
+            var meeting = await _context.Meetings.FindAsync(id);
+            if (meeting == null)
+            {
+                return NotFound();
+            }
+
+            _context.Meetings.Remove(meeting);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool MeetingExists(int id)
+        {
+            return _context.Meetings.Any(e => e.Id == id);
+        }
+    }
+}
